b1de42f849fb9f1c8f06eea4199da0d8fa063277
Rutvik-C <rutvikchoudhari97@gmail.com>
2021-04-05 15:45:42 +0530

    minor changes

--- app.py
+++ app.py
@@ -2,17 +2,22 @@ from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, reqparse
 import pickle
 import pandas as pd
+import firebase_admin
+from firebase_admin import credentials, firestore
 import sklearn
 
 app = Flask(__name__)
 api = Api(app)
+
+cred = credentials.Certificate("./credentials/firebase_creds.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 user_rec_args = reqparse.RequestParser()
 user_rec_args.add_argument("user-uid", type=str, required=True)
 
 sim_rec_args = reqparse.RequestParser()
 sim_rec_args.add_argument("item-name", type=str, required=True)
-
 
 model = pickle.load(open("./model/food_recommendation.pkl", "rb"))
 
@@ -24,20 +29,31 @@ features = df.drop(["name"], axis=1)
 class UserRecommendation(Resource):
     def get(self):
         args = user_rec_args.parse_args()
-        return jsonify({"side": "user", "your-request": args})
+        user_id = args["user-uid"]
 
-        # try:
-        #     test = features.iloc[food_index]
-        #
-        #     rec = list()
-        #     dist, ind = model.kneighbors([test], n_neighbors=6)
-        #     for i in ind[0][1:]:
-        #         rec.append(names[i])
-        #
-        #     return jsonify({"result": "success", "recommendation": rec})
-        #
-        # except Exception as e:
-        #     return jsonify({"result": "failed", "error_message": e})
+        doc_ref = db.collection("Users").document(user_id)
+        doc = doc_ref.get()
+
+        if doc.exists:
+            doc = doc.to_dict()
+
+            user_feature = doc["rec_arr"]
+            user_feature.insert(0, doc["spice"])
+            user_feature.insert(0, doc["preference"])
+
+            rec = list()
+            dist, ind = model.kneighbors([user_feature], n_neighbors=11)
+            for i in ind[0][1:]:
+                rec.append(names[i])
+
+            resp = jsonify({"recommendation": rec})
+            resp.status_code = 201
+            return resp
+
+        else:
+            resp = jsonify({"message": "user not found"})
+            resp.status_code = 400
+            return resp
 
 
 class SimilarRecommendation(Resource):
@@ -55,13 +71,19 @@ class SimilarRecommendation(Resource):
                 for i in ind[0][1:]:
                     rec.append(names[i])
 
-                return jsonify({"result": "success", "message": rec, "target": item_name})
+                resp = jsonify({"recommendation": rec})
+                resp.status_code = 201
+                return resp
 
             else:
-                return jsonify({"result": "error", "message": "item not found"})
+                resp = jsonify({"message": "item not found"})
+                resp.status_code = 400
+                return resp
 
         except Exception as e:
-            return jsonify({"result": "error", "message": e})
+            resp = jsonify({"message": e})
+            resp.status_code = 400
+            return resp
 
 
 api.add_resource(UserRecommendation, "/user-recommendation")
